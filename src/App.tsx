548a4e053d1a30be86d012b0ec65543d17d162fa
LitvinenkoBD <boris12lit@gmail.com>
2022-08-17 20:36:44 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+    const hi:string = "Well, hello. I'm React 17.0.2 with TS template..."
+
+    return (
+        <div className="App">
+            <div className="appWrapper" style={{textAlign: "center", fontSize: 22}}>
+                <h1>{hi}</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+        <App/>,
+    document.getElementById('root')
+);
